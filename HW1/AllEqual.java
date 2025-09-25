1c3c6e19198cc572eb7c78e68cada5297c45687d
DinhIT0826 <24020067@vnu.edu.vn>
2025-09-25 22:51:55 +0700

    baitapC

---
+++ HW1/AllEqual.java
@@ -0,0 +1,13 @@
+public class AllEqual {
+    public static void main(String[] args) {
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        int c = Integer.parseInt(args[2]);
+        if (a == b && a == c) {
+            System.out.println("all equal");
+        }
+        else {
+            System.out.println("not all equal");
+        }
+    }
+}
---
+++ HW1/Introduction.java
@@ -0,0 +1,10 @@
+public class Introduction {
+    public static void main(String[] args) {
+        System.out.println("Bùi Văn Đình \t 24020067 \t QH-2024-I/CQ-I-IT4 \t vdin0826 \t 24020067@vnu.edu.vn");
+        for(int i = 9 ; i >= 1; i--) {
+            System.out.println(i + "bottles of beer on the wall, " + i + " bottles of beer.");
+        }
+        System.out.println("No more bottles of beer on the wall.");
+    }
+}
+
