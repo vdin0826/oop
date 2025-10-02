8835da218993e69453fa9118721635e3c19f1e7e
DinhIT0826 <24020067@vnu.edu.vn>
2025-10-02 23:46:38 +0700

    HW2

--- HW2/src/main/java/StringCode.java
+++ HW2/src/main/java/StringCode.java
@@ -12,7 +12,20 @@ public class StringCode {
 	 * @return max run length
 	 */
 	public static int maxRun(String str) {
-		return 0; // YOUR CODE HERE
+        if (str.length() == 0) {
+            return 0;
+        }
+        int max = 1;
+        int count = 1;
+        for (int i = 1 ; i < str.length() ; i++) {
+            if (str.charAt(i) == str.charAt(i - 1)) {
+                count ++;
+            } else {
+                count = 1;
+            }
+            max = Math.max(max, count);
+        }
+        return max;// YOUR CODE HERE
 	}
 
 	
@@ -24,7 +37,22 @@ public class StringCode {
 	 * @return blown up string
 	 */
 	public static String blowup(String str) {
-		return null; // YOUR CODE HERE
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (Character.isDigit(c)) {
+                int num = c - '0';
+                if (i + 1 < str.length()) {
+                    char next = str.charAt(i + 1);
+                    for (int j = 0; j < num; j++) {
+                        result.append(next);
+                    }
+                }
+            } else {
+                result.append(c);
+            }
+        }
+        return result.toString(); // YOUR CODE HERE
 	}
 	
 	/**
@@ -34,6 +62,22 @@ public class StringCode {
 	 * Compute this in linear time using a HashSet. Len will be 1 or more.
 	 */
 	public static boolean stringIntersect(String a, String b, int len) {
-		return false; // YOUR CODE HERE
+        if (len <= 0 || a.length() < len || b.length() < len) {
+            return false;
+        }
+
+        HashSet<String> set = new HashSet<>();
+
+        for (int i = 0; i <= a.length() - len; i++) {
+            set.add(a.substring(i, i + len));
+        }
+
+        for (int i = 0; i <= b.length() - len; i++) {
+            if (set.contains(b.substring(i, i + len))) {
+                return true;
+            }
+        }
+
+        return false; // YOUR CODE HERE
 	}
 }
